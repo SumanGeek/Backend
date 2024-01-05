ad01ee3d04efc089e7253d169c6b1d8c55f38de5
SumanGeek <niroulas542@gmail.com>
2024-01-05 20:57:11 +0545

    git added

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-
+const person = {
+  name: "Suman",
+  Address: "Biratnagar",
+  Age: "23",
+  " Occupation": "Software Engineer",
+};
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -11,6 +16,9 @@ app.get("/home", (req, res) => {
   res.send("THis is a home page");
 });
 
+app.get("/person", (req, res) => {
+  res.json(person);
+});
 app.get("/login", (req, res) => {
   res.send("<h1>Login Page</h1>");
 });
